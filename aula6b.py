b2f0c19718911e7e43efd627fccd91956e0c4763
fabioromaobarreto <romaobarreto@hotmail.com>
2020-12-15 00:28:12 -0300

    Avançando em Python
    Estudando Python de forma intensiva!

---
+++ aula6b.py
@@ -0,0 +1,4 @@
+from math import hypot
+co = int(input("CATETO OPOSTO: "))
+ca = int(input("CATETO ADJACENTE: "))
+print(f"HIPOTENUSA: {hypot(co, ca)}")
---
+++ aula6c.py
@@ -0,0 +1,3 @@
+from math import sin, cos, tan,radians
+ang = int(input("ÂNGULO: "))
+print(f"SENO: {sin(radians(ang)):.3f}\nCOSSENO: {cos(radians(ang)):.3f}\nTANGENTE: {tan(radians(ang)):.3f}")
