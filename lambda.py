86c14e473b24ca107ccbf40f83451508379d3b7a
fabioromaobarreto <romaobarreto@hotmail.com>
2021-01-03 22:44:34 -0300

    lambda

---
+++ lambda.py
@@ -0,0 +1,16 @@
+
+# LAMBDA
+"""valor = lambda x, y: x*y
+
+print(valor(2, 3))"""
+
+# MAP
+"""lista = [1, 2, 3, 4, 5]
+
+
+valor = list(map(lambda x: x**2, lista))
+print(valor)"""
+
+# FILTER
+me = list(filter(lambda item: item % 2 == 0, [1, 2, 3, 4, 5]))
+print(me)
