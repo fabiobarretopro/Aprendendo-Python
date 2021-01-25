4d82e97f73f1c9d71ec31d9fd0651f5cb8935c64
fabiobarreto_pro <fabiobarreto_pro@hotmail.com>
2021-01-24 21:08:34 -0300

    Composição

--- POO/main.py
+++ POO/main.py
@@ -1,10 +1,21 @@
-from classes import Escritor, Caneta, MaquinaDeEscrever
+from composição import Cliente
 
-
-escritor = Escritor("João Ubaldo Ribeiro")
-caneta = Caneta("BIC")
-maquina = MaquinaDeEscrever()
-
-escritor.ferramenta = caneta
-escritor.ferramenta.escrever()
-
+c1 = Cliente("Fábio", 40)
+c1.insere_endereco("Salvador", "BA")
+print(c1.nome)
+c1.lista_enderecos()
+del c1
+print("===============================================")
+c2 = Cliente("Maria Clara", 5)
+c2.insere_endereco("Lauro de Freitas", "BA")
+c2.insere_endereco("Salvador", "BA")
+print(c2.nome)
+c2.lista_enderecos()
+del c2
+print("===============================================")
+c3 = Cliente("Ana Júlia", 32)
+c3.insere_endereco("Feira de Santana", "BA")
+print(c3.nome)
+c3.lista_enderecos()
+del c3
+print("######################################################")
